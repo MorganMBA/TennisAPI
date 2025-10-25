15834be5aedbad2f714a27f50109a15179605753
MorganMBA <morganmba5@gmail.com>
2025-10-25 19:51:21 +0200

    add dynamo nuget package and initialize dynomo service

--- Tennis.API/Program.cs
+++ Tennis.API/Program.cs
@@ -1,11 +1,19 @@
+using Amazon.DynamoDBv2;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAWSService<IAmazonDynamoDB>(new Amazon.Extensions.NETCore.Setup.AWSOptions()
+{
+    Region = Amazon.RegionEndpoint.EUCentral1
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
