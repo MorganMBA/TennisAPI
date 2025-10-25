3ed7d8dcae915cec9c9d0f2ecfc2c30fbc11ef05
MorganMBA <morganmba5@gmail.com>
2025-10-26 00:02:14 +0200

    adding mapper and player service

---
+++ Tennis.Domain/Mapper/PlayerMapper.cs
@@ -0,0 +1,56 @@
+﻿using Tennis.Core.Dtos;
+using Tennis.Core.Entities;
+
+namespace Tennis.Domain.Mapper
+{
+    public static class PlayerMapper
+    {
+        public static PlayerDto ToDto(Player entity)
+        {
+            return new PlayerDto
+            {
+                Id = entity.Id,
+                Firstname = entity.Firstname,
+                Lastname = entity.Lastname,
+                Shortname = entity.Shortname,
+                Sex = entity.Sex,
+                CountryCode = entity.Country.Code,
+                CountryPicture = entity.Country.Picture,
+                Picture = entity.Picture,
+                Rank = entity.Data.Rank,
+                Points = entity.Data.Points,
+                Weight = entity.Data.Weight,
+                Height = entity.Data.Height,
+                Age = entity.Data.Age,
+                Last = entity.Data.Last
+            };
+        }
+
+        public static Player ToEntity(PlayerDto dto)
+        {
+            return new Player
+            {
+                Id = dto.Id,
+                Firstname = dto.Firstname,
+                Lastname = dto.Lastname,
+                Shortname = dto.Shortname,
+                Sex = dto.Sex,
+                Picture = dto.Picture,
+                Country = new Country
+                {
+                    Code = dto.CountryCode,
+                    Picture = dto.CountryPicture
+                },
+                Data = new PlayerData
+                {
+                    Rank = dto.Rank,
+                    Points = dto.Points,
+                    Weight = dto.Weight,
+                    Height = dto.Height,
+                    Age = dto.Age,
+                    Last = dto.Last
+                }
+            };
+        }
+    }
+}
---
+++ Tennis.Domain/Services/Impl/PlayerService.cs
@@ -0,0 +1,31 @@
+﻿using Tennis.Core.Dtos;
+using Tennis.Core.Repositories;
+using Tennis.Domain.Mapper;
+using Tennis.Domain.Services.Interfaces;
+
+namespace Tennis.Domain.Services.Impl
+{
+    public class PlayerService : IPlayerService
+    {
+        private readonly IPlayerRepository _playerRepository;
+        public PlayerService(IPlayerRepository playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
+        public async Task AddAsync(PlayerDto playerDto)
+        {
+            var player = PlayerMapper.ToEntity(playerDto);
+            await _playerRepository.AddAsync(player);
+        }
+
+        public Task<IEnumerable<PlayerDto>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PlayerDto?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
